a97098bf50fe9843f67f7bfeca066dfc9a4979f3
João Guilherme <joaog95@live.com>
2020-10-25 00:40:26 -0300

    feat: criado um projeto React para integrar ao micro-frontend

--- single-spa/src/jg-root-config.js
+++ single-spa/src/jg-root-config.js
@@ -6,7 +6,14 @@ registerApplication({
     System.import(
       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
     ),
-  activeWhen: ["/"],
+  activeWhen: (location) => location.pathname === "/",
+});
+
+registerApplication({
+  name: "@jg/react-single",
+  app: () =>
+    System.import("@jg/react-single"),
+  activeWhen: (location) => location.pathname === "/react-single",
 });
 
 start({
