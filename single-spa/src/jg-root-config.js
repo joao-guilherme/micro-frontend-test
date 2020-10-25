6d19dcd82d286b67e5a39dcc131330e34d041308
João Guilherme <joaog95@live.com>
2020-10-25 17:53:08 -0300

    feat: deixando a Welcome screen como um default registerApplication

--- single-spa/src/jg-root-config.js
+++ single-spa/src/jg-root-config.js
@@ -2,24 +2,19 @@ import { registerApplication, start } from "single-spa";
 
 registerApplication({
   name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
+  app: () => System.import("@single-spa/welcome"),
   activeWhen: (location) => location.pathname === "/",
 });
 
 registerApplication({
   name: "@jg/react-single",
-  app: () =>
-    System.import("@jg/react-single"),
+  app: () => System.import("@jg/react-single"),
   activeWhen: (location) => location.pathname === "/react-single",
 });
 
 registerApplication({
   name: "@jg/react-multiple",
-  app: () =>
-    System.import("@jg/react-multiple"),
+  app: () => System.import("@jg/react-multiple"),
   activeWhen: ["/react-multiple"],
 });
 
