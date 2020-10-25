b0562f08e4a711c3a2c188cf39859cf082e4e8c8
João Guilherme <joaog95@live.com>
2020-10-25 00:23:27 -0300

    feat: criando um novo projeto micro-frontend para aprendizado

---
+++ single-spa/src/jg-root-config.js
@@ -0,0 +1,14 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@single-spa/welcome",
+  app: () =>
+    System.import(
+      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+    ),
+  activeWhen: ["/"],
+});
+
+start({
+  urlRerouteOnly: true,
+});
